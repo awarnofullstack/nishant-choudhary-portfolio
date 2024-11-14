d1d0ad7163a932f0bb9f538268dae6fc2d713e62
abhishek <awarnofullstack@gmail.com>
2024-11-14 09:12:13 +0530

    push mobile responsive edits

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const swiper = new Swiper('.swiper-custom-index', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    autoplay: {
-      delay: 5000,
-    },
+    // autoplay: {
+    //   delay: 5000,
+    // },
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
@@ -16,7 +16,13 @@ const swiper = new Swiper('.swiper-custom-index', {
             const activeSlide = this.slides[this.activeIndex];
             // Get the background color from data attribute
             const color = activeSlide.getAttribute('data-color');
+            const image = activeSlide.getAttribute('data-image');
             
+            if(image === "true"){
+              document.body.style.backgroundImage = "url('./assets/img/mobile-home-bg.png')";
+            }else{
+              document.body.style.backgroundImage = "";
+            }
             // Apply the color to the body background
             document.body.style.backgroundColor = color;
         },
